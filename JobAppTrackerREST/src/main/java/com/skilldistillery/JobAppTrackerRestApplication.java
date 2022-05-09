8140948c6e5698b71114a03bc1ac490e0757ad80
DustinMC1802 <dustinmcollier@gmail.com>
2022-05-09 07:44:11 -0700

    README updated

--- JobAppTrackerREST/src/main/java/com/skilldistillery/JobAppTrackerRestApplication.java
+++ JobAppTrackerREST/src/main/java/com/skilldistillery/JobAppTrackerRestApplication.java
@@ -2,10 +2,17 @@ package com.skilldistillery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobAppTrackerRestApplication {
+public class JobAppTrackerRestApplication extends SpringBootServletInitializer{
 
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(JobAppTrackerRestApplication.class);
+	  }
+	  
 	public static void main(String[] args) {
 		SpringApplication.run(JobAppTrackerRestApplication.class, args);
 	}
